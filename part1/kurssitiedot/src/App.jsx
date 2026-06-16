0e997115755ddda4effbd9eb7bfd9744acdf2880
Jeremia Vepsäläinen <Jeremia.vepsalainen@gmail.com>
2026-06-16 09:45:34 +0300

    osa1, tehtävä 1 tehty

---
+++ part1/kurssitiedot/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.name1} {props.exercise1}
+      </p>
+      <p>
+        {props.name2} {props.exercise2}
+      </p>
+      <p>
+        {props.name3} {props.exercise3}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.number1 + props.number2 + props.number3}</p>
+  );
+};
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        name1={part1}
+        exercise1={exercises1}
+        name2={part2}
+        exercise2={exercises2}
+        name3={part3}
+        exercise3={exercises3}
+      ></Content>
+      <Total
+        number1={exercises1}
+        number2={exercises2}
+        number3={exercises3}
+      ></Total>
+    </div>
+  );
+};
+
+export default App;
