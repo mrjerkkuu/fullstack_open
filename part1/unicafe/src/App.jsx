5c19a1e2ea98c78982b14a76414415c33181c48c
Jeremia Vepsäläinen <Jeremia.vepsalainen@gmail.com>
2026-07-13 11:07:55 +0300

    tehtävä 1.6 tehty

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give Feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text='good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='bad' />
+
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+};
+
+export default App;
