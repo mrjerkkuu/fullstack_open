f6971d9305980d162599e50d473572ccc66aa1b4
Jeremia Vepsäläinen <Jeremia.vepsalainen@gmail.com>
2026-07-13 11:47:06 +0300

    1.9 tehty

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,6 +3,15 @@ import { useState } from 'react';
 const Statistics = (props) => {
   const { good, neutral, bad, all, average, positive } = props;
 
+  if (all === 0) {
+    return (
+      <div>
+        {' '}
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
   return (
     <div>
       <h2>statistics</h2>
